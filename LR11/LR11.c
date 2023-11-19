ac7a54b4a4548f9bb2541d4c4b8772fbfbb7d4bd
luckyabatur <144679552+luckyabatur@users.noreply.github.com>
2023-11-19 15:10:47 +0300

    Add files via upload

---
+++ LR11/LR11.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int state = 0, count = 0;
+    char s;
+    while ((scanf("%c", &s)) != EOF)
+    {
+        switch (state)
+        {
+            case 0:
+                if (s != ' ' && s != '\n')
+                {
+                    state = 1;
+                }
+                break;
+            case 1:
+                if (s != ' ' && s != '\n')
+                {
+                    state = 2;
+                }
+                else
+                {
+                    state = 0;
+                }
+                break;
+            case 2:
+                if (s != ' ' && s != '\n')
+                {
+                    count += 1;
+                    state = 3;
+                }
+                else
+                {
+                    state = 0;
+                }
+                break;
+            case 3:
+                if (s == ' ' || s == '\n')
+                {
+                    state = 0;
+                }
+        }
+    }
+    printf("\nКоличесво слов, содержащих не менее 3-х букв: %d", count);
+}
